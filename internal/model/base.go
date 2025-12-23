9b61c02674fc507a8f8b5b65628dad4a2d59508a
wxl <gopher095@gmail.com>
2025-12-23 13:36:44 +0800

    style: 优化 BaseDO 结构体字段顺序
    调整字段顺序以提高可读性和一致性：
    - Creator 和 Updater 相关字段相邻
    - CreateTime 和 UpdateTime 时间字段相邻
    - 逻辑上相关的字段放在一起

--- internal/model/base.go
+++ internal/model/base.go
@@ -7,8 +7,8 @@ import (
 // BaseDO 基础实体对象
 type BaseDO struct {
 	Creator    string    `gorm:"column:creator;size:64;default:'';comment:创建者" json:"creator"`
-	CreateTime time.Time `gorm:"column:create_time;autoCreateTime;comment:创建时间" json:"createTime"`
 	Updater    string    `gorm:"column:updater;size:64;default:'';comment:更新者" json:"updater"`
+	CreateTime time.Time `gorm:"column:create_time;autoCreateTime;comment:创建时间" json:"createTime"`
 	UpdateTime time.Time `gorm:"column:update_time;autoUpdateTime;comment:更新时间" json:"updateTime"`
 	Deleted    BitBool   `gorm:"column:deleted;softDelete:flag;default:0;comment:是否删除" json:"deleted"`
 }
