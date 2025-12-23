b0f26f0be0c4ba240b3f275f55e3db3dde70c99e
wxl <gopher095@gmail.com>
2025-12-23 09:30:13 +0800

    style(model): 规范交易配送模型字段格式对齐
    - 调整TradeDeliveryPickUpStore结构体字段类型声明的对齐方式
    - 删除不必要的Sort字段注释
    - 提高代码可读性

--- internal/model/trade/delivery.go
+++ internal/model/trade/delivery.go
@@ -28,26 +28,25 @@ func (TradeDeliveryExpress) TableName() string {
 
 // TradeDeliveryPickUpStore 自提门店 DO
 type TradeDeliveryPickUpStore struct {
-	ID            int64         `gorm:"primaryKey;autoIncrement;comment:编号" json:"id"`
-	Name          string        `gorm:"size:64;not null;comment:门店名称" json:"name"`
-	Introduction  string        `gorm:"size:256;default:'';comment:门店简介" json:"introduction"`
-	Phone         string        `gorm:"size:11;not null;comment:联系电话" json:"phone"`
-	AreaID        int           `gorm:"column:area_id;not null;comment:区域编号" json:"areaId"`
-	DetailAddress string        `gorm:"size:256;not null;comment:详细地址" json:"detailAddress"`
-	Logo          string        `gorm:"size:256;not null;comment:门店Logo" json:"logo"`
-	OpeningTime   *time.Time    `gorm:"column:opening_time;comment:营业开始时间" json:"openingTime"`
-	ClosingTime   *time.Time    `gorm:"column:closing_time;comment:营业结束时间" json:"closingTime"`
-	Latitude      float64       `gorm:"type:decimal(10,6);comment:纬度" json:"latitude"`
-	Longitude     float64       `gorm:"type:decimal(10,6);comment:经度" json:"longitude"`
+	ID            int64                `gorm:"primaryKey;autoIncrement;comment:编号" json:"id"`
+	Name          string               `gorm:"size:64;not null;comment:门店名称" json:"name"`
+	Introduction  string               `gorm:"size:256;default:'';comment:门店简介" json:"introduction"`
+	Phone         string               `gorm:"size:11;not null;comment:联系电话" json:"phone"`
+	AreaID        int                  `gorm:"column:area_id;not null;comment:区域编号" json:"areaId"`
+	DetailAddress string               `gorm:"size:256;not null;comment:详细地址" json:"detailAddress"`
+	Logo          string               `gorm:"size:256;not null;comment:门店Logo" json:"logo"`
+	OpeningTime   *time.Time           `gorm:"column:opening_time;comment:营业开始时间" json:"openingTime"`
+	ClosingTime   *time.Time           `gorm:"column:closing_time;comment:营业结束时间" json:"closingTime"`
+	Latitude      float64              `gorm:"type:decimal(10,6);comment:纬度" json:"latitude"`
+	Longitude     float64              `gorm:"type:decimal(10,6);comment:经度" json:"longitude"`
 	VerifyUserIds model.IntListFromCSV `gorm:"column:verify_user_ids;type:varchar(500);comment:核销员工用户编号数组" json:"verifyUserIds"`
-	Status        int           `gorm:"default:0;not null;comment:状态" json:"status"`
-	Sort          int           `gorm:"default:0;not null;comment:排序" json:"sort"`
-	Creator       string        `gorm:"size:64;default:'';comment:创建者" json:"creator"`
-	Updater       string        `gorm:"size:64;default:'';comment:更新者" json:"updater"`
-	CreatedAt     time.Time     `gorm:"column:create_time;autoCreateTime;comment:创建时间" json:"createTime"`
-	UpdatedAt     time.Time     `gorm:"column:update_time;autoUpdateTime;comment:更新时间" json:"updateTime"`
-	Deleted       model.BitBool `gorm:"column:deleted;softDelete:flag;default:0;comment:是否删除" json:"deleted"`
-	TenantID      int64         `gorm:"column:tenant_id;default:0;comment:租户编号" json:"tenantId"`
+	Status        int                  `gorm:"default:0;not null;comment:状态" json:"status"`
+	Creator       string               `gorm:"size:64;default:'';comment:创建者" json:"creator"`
+	Updater       string               `gorm:"size:64;default:'';comment:更新者" json:"updater"`
+	CreatedAt     time.Time            `gorm:"column:create_time;autoCreateTime;comment:创建时间" json:"createTime"`
+	UpdatedAt     time.Time            `gorm:"column:update_time;autoUpdateTime;comment:更新时间" json:"updateTime"`
+	Deleted       model.BitBool        `gorm:"column:deleted;softDelete:flag;default:0;comment:是否删除" json:"deleted"`
+	TenantID      int64                `gorm:"column:tenant_id;default:0;comment:租户编号" json:"tenantId"`
 }
 
 func (TradeDeliveryPickUpStore) TableName() string {
