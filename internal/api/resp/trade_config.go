c063506c3517741e2fd83cc429556631cd282b49
wxl <gopher095@gmail.com>
2025-12-26 20:11:00 +0800

    feat(trade): 增强交易配置模型支持售后和物流配置
    - 新增售后退款理由和退货理由配置（JSON 数组）
    - 新增全场包邮功能配置（启用状态、最小金额）
    - 新增自提功能配置选项
    - 优化分销海报图字段类型为 JSON 数组
    - 新增提现方式配置字段（支持 CSV 列表）
    - 更新相关请求/响应 DTO 同步变更

--- internal/api/resp/trade_config.go
+++ internal/api/resp/trade_config.go
@@ -1,12 +1,18 @@
 package resp
 
+// TradeConfigResp Admin 端 - 交易配置响应 (对齐 Java: TradeConfigRespVO)
 type TradeConfigResp struct {
-	ID                          int64    `json:"id"`
+	ID                          int64    `json:"id"`                          // 自增主键
 	AppID                       int64    `json:"appId"`                       // 支付应用 ID
 	AfterSaleDeadlineDays       int      `json:"afterSaleDeadlineDays"`       // 售后期限(天)
 	PayTimeoutMinutes           int      `json:"payTimeoutMinutes"`           // 支付超时(分钟)
 	AutoReceiveDays             int      `json:"autoReceiveDays"`             // 自动收货(天)
 	AutoCommentDays             int      `json:"autoCommentDays"`             // 自动好评(天)
+	AfterSaleRefundReasons      []string `json:"afterSaleRefundReasons"`      // 售后的退款理由
+	AfterSaleReturnReasons      []string `json:"afterSaleReturnReasons"`      // 售后的退货理由
+	DeliveryExpressFreeEnabled  bool     `json:"deliveryExpressFreeEnabled"`  // 是否启用全场包邮
+	DeliveryExpressFreePrice    int      `json:"deliveryExpressFreePrice"`    // 全场包邮的最小金额
+	DeliveryPickUpEnabled       bool     `json:"deliveryPickUpEnabled"`       // 是否开启自提
 	BrokerageWithdrawMinPrice   int      `json:"brokerageWithdrawMinPrice"`   // 提现最低金额
 	BrokerageWithdrawFeePercent int      `json:"brokerageWithdrawFeePercent"` // 提现手续费百分比
 	BrokerageEnabled            bool     `json:"brokerageEnabled"`            // 是否开启分销
@@ -16,4 +22,18 @@ type TradeConfigResp struct {
 	BrokerageEnabledCondition   int      `json:"brokerageEnabledCondition"`   // 分销资格启用条件 1:人人分销 2:仅指定用户
 	BrokerageBindMode           int      `json:"brokerageBindMode"`           // 分销关系绑定模式 1:首次绑定 2:注册绑定 3:覆盖绑定
 	BrokeragePosterUrls         []string `json:"brokeragePosterUrls"`         // 分销海报图
+	BrokerageWithdrawTypes      []int    `json:"brokerageWithdrawTypes"`      // 提现方式列表
+	TencentLbsKey               string   `json:"tencentLbsKey"`               // 腾讯地图 Key
+}
+
+// AppTradeConfigResp App 端 - 交易配置响应 (对齐 Java: AppTradeConfigRespVO)
+type AppTradeConfigResp struct {
+	TencentLbsKey             string   `json:"tencentLbsKey"`             // 腾讯地图 Key
+	DeliveryPickUpEnabled     bool     `json:"deliveryPickUpEnabled"`     // 是否启用自提
+	AfterSaleRefundReasons    []string `json:"afterSaleRefundReasons"`    // 售后退款原因列表
+	AfterSaleReturnReasons    []string `json:"afterSaleReturnReasons"`    // 售后退货原因列表
+	BrokeragePosterUrls       []string `json:"brokeragePosterUrls"`       // 分销海报图
+	BrokerageFrozenDays       int      `json:"brokerageFrozenDays"`       // 分销佣金冻结时间（天）
+	BrokerageWithdrawMinPrice int      `json:"brokerageWithdrawMinPrice"` // 提现最低金额
+	BrokerageWithdrawTypes    []int    `json:"brokerageWithdrawTypes"`    // 提现方式列表
 }
--- internal/model/trade/config.go
+++ internal/model/trade/config.go
@@ -2,26 +2,34 @@ package trade
 
 import (
 	"github.com/wxlbd/ruoyi-mall-go/internal/model"
+	"github.com/wxlbd/ruoyi-mall-go/pkg/types"
+	"gorm.io/datatypes"
 )
 
 // TradeConfig 交易中心 - 交易配置
 // Table: trade_config
 type TradeConfig struct {
-	ID                          int64         `gorm:"primaryKey;autoIncrement;comment:主键" json:"id"`
-	AppID                       int64         `gorm:"column:app_id;not null;comment:支付应用ID" json:"appId"`
-	AfterSaleDeadlineDays       int           `gorm:"column:after_sale_deadline_days;not null;comment:售后期限(天)" json:"afterSaleDeadlineDays"`
-	PayTimeoutMinutes           int           `gorm:"column:pay_timeout_minutes;not null;comment:支付超时(分钟)" json:"payTimeoutMinutes"`
-	AutoReceiveDays             int           `gorm:"column:auto_receive_days;not null;comment:自动收货(天)" json:"autoReceiveDays"`
-	AutoCommentDays             int           `gorm:"column:auto_comment_days;not null;comment:自动好评(天)" json:"autoCommentDays"`
-	BrokerageWithdrawMinPrice   int           `gorm:"column:brokerage_withdraw_min_price;default:0;comment:提现最低金额" json:"brokerageWithdrawMinPrice"`
-	BrokerageWithdrawFeePercent int           `gorm:"column:brokerage_withdraw_fee_percent;default:0;comment:提现手续费百分比" json:"brokerageWithdrawFeePercent"`
-	BrokerageEnabled            model.BitBool `gorm:"column:brokerage_enabled;default:0;comment:是否开启分销" json:"brokerageEnabled"`
-	BrokerageFrozenDays         int           `gorm:"column:brokerage_frozen_days;default:0;comment:分销佣金冻结时间" json:"brokerageFrozenDays"`
-	BrokerageFirstPercent       int           `gorm:"column:brokerage_first_percent;default:0;comment:一级分销比例" json:"brokerageFirstPercent"`
-	BrokerageSecondPercent      int           `gorm:"column:brokerage_second_percent;default:0;comment:二级分销比例" json:"brokerageSecondPercent"`
-	BrokerageEnabledCondition   int           `gorm:"column:brokerage_enabled_condition;default:1;comment:分销资格启用条件 1:人人分销 2:仅指定用户" json:"brokerageEnabledCondition"`
-	BrokerageBindMode           int           `gorm:"column:brokerage_bind_mode;default:1;comment:分销关系绑定模式 1:首次绑定 2:注册绑定 3:覆盖绑定" json:"brokerageBindMode"`
-	BrokeragePosterUrls         string        `gorm:"column:brokerage_poster_urls;default:'';comment:分销海报图" json:"brokeragePosterUrls"`
+	ID                          int64                       `gorm:"primaryKey;autoIncrement;comment:主键" json:"id"`
+	AppID                       int64                       `gorm:"column:app_id;not null;comment:支付应用ID" json:"appId"`
+	AfterSaleDeadlineDays       int                         `gorm:"column:after_sale_deadline_days;not null;comment:售后期限(天)" json:"afterSaleDeadlineDays"`
+	PayTimeoutMinutes           int                         `gorm:"column:pay_timeout_minutes;not null;comment:支付超时(分钟)" json:"payTimeoutMinutes"`
+	AutoReceiveDays             int                         `gorm:"column:auto_receive_days;not null;comment:自动收货(天)" json:"autoReceiveDays"`
+	AutoCommentDays             int                         `gorm:"column:auto_comment_days;not null;comment:自动好评(天)" json:"autoCommentDays"`
+	AfterSaleRefundReasons      datatypes.JSONSlice[string] `gorm:"column:after_sale_refund_reasons;type:json;comment:售后的退款理由" json:"afterSaleRefundReasons"`
+	AfterSaleReturnReasons      datatypes.JSONSlice[string] `gorm:"column:after_sale_return_reasons;type:json;comment:售后的退货理由" json:"afterSaleReturnReasons"`
+	DeliveryExpressFreeEnabled  model.BitBool               `gorm:"column:delivery_express_free_enabled;default:0;comment:是否启用全场包邮" json:"deliveryExpressFreeEnabled"`
+	DeliveryExpressFreePrice    int                         `gorm:"column:delivery_express_free_price;default:0;comment:全场包邮的最小金额" json:"deliveryExpressFreePrice"`
+	DeliveryPickUpEnabled       model.BitBool               `gorm:"column:delivery_pick_up_enabled;default:0;comment:是否开启自提" json:"deliveryPickUpEnabled"`
+	BrokerageWithdrawMinPrice   int                         `gorm:"column:brokerage_withdraw_min_price;default:0;comment:提现最低金额" json:"brokerageWithdrawMinPrice"`
+	BrokerageWithdrawFeePercent int                         `gorm:"column:brokerage_withdraw_fee_percent;default:0;comment:提现手续费百分比" json:"brokerageWithdrawFeePercent"`
+	BrokerageEnabled            model.BitBool               `gorm:"column:brokerage_enabled;default:0;comment:是否开启分销" json:"brokerageEnabled"`
+	BrokerageFrozenDays         int                         `gorm:"column:brokerage_frozen_days;default:0;comment:分销佣金冻结时间" json:"brokerageFrozenDays"`
+	BrokerageFirstPercent       int                         `gorm:"column:brokerage_first_percent;default:0;comment:一级分销比例" json:"brokerageFirstPercent"`
+	BrokerageSecondPercent      int                         `gorm:"column:brokerage_second_percent;default:0;comment:二级分销比例" json:"brokerageSecondPercent"`
+	BrokerageEnabledCondition   int                         `gorm:"column:brokerage_enabled_condition;default:1;comment:分销资格启用条件 1:人人分销 2:仅指定用户" json:"brokerageEnabledCondition"`
+	BrokerageBindMode           int                         `gorm:"column:brokerage_bind_mode;default:1;comment:分销关系绑定模式 1:首次绑定 2:注册绑定 3:覆盖绑定" json:"brokerageBindMode"`
+	BrokeragePosterUrls         datatypes.JSONSlice[string] `gorm:"column:brokerage_poster_urls;type:json;comment:分销海报图地址数组" json:"brokeragePosterUrls"`
+	BrokerageWithdrawTypes      types.ListFromCSV[int]      `gorm:"column:brokerage_withdraw_types;type:varchar(255);comment:提现方式" json:"brokerageWithdrawTypes"`
 	model.TenantBaseDO
 }
 
