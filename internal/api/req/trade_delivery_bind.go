b2686c18b4eaa301b07f6ae7e6e8ba5bb06a0bd2
wxl <gopher095@gmail.com>
2025-12-31 13:59:04 +0800

    实现层级化 handler

--- internal/api/req/trade_delivery_bind.go
+++
@@ -1,7 +0,0 @@
-package req
-
-// DeliveryPickUpBindReq 自提门店绑定核销员工 Request
-type DeliveryPickUpBindReq struct {
-	ID            int64   `json:"id" binding:"required"`
-	VerifyUserIds []int64 `json:"verifyUserIds" binding:"required"`
-}
--- internal/api/req/trade_statistics.go
+++
@@ -1,8 +0,0 @@
-package req
-
-import "time"
-
-// TradeStatisticsAnalysisReq 交易状况分析 Request
-type TradeStatisticsAnalysisReq struct {
-	Times []time.Time `form:"times[]" time_format:"2006-01-02 15:04:05"` // 时间范围 [start, end]
-}
